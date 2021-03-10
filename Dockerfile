e33e8cb326fe7933af985e15ea96a478c7790279
William Tenório <contato@williamtenorio.com.br>
2021-03-10 13:17:02 +0000

    primeiro comit

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
-FROM node:latest
+FROM node:alpine
 
 LABEL maintainer "William Tenório da Silva <contato@williamtenorio.com.br>"
-
-RUN mkdir -p /usr/src/app/node_modules && chmod -R 775 /usr/src/app && chmod -R 775 /usr/local/lib/node_modules
 
 WORKDIR /usr/src/app/
 
 COPY package*.json ./
 
-RUN npm install -g nodemon && npm install
+RUN npm install
 
 COPY . .
 
--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 require('dotenv/config');
 const express = require('express');
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
-const HOST = process.env.APP_URL;
-const PORT = process.env.APP_PORT;
+const {
+  APP_URL,
+  APP_PORT,
+  DB_DATABASE,
+  DB_HOSTNAME,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD
+} = process.env;
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
-})
- 
-app.listen(3000);
+  res.send('Se inscreva no meu canal!!!');
+});
 
-console.log(`Running on ${HOST}:${PORT}`);
+MongoClient.connect(
+  `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result => {
+    console.log('MongoDB Connect!!!');
+  })
+  .catch(error => {
+    console.log('MongoDB Fail!!!');
+  });
+
+app.listen(APP_PORT);
+
+console.log(`Setup NoMoreBug [ON]: ${APP_URL}:${APP_PORT}`);
+
